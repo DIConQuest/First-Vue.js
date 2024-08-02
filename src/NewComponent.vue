f7207055dee33be0cc7b73e3b70ff85a78b64106
DIConQuest <bobosgeist@naver.com>
2024-08-02 19:10:45 +0900

    add component

---
+++ src/NewComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="alert-box">
+    <p>New component here</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewComponent",
+};
+</script>
+
+<style>
+.alert-box {
+  width: 50vw;
+  height: 40px;
+  color: #eee;
+  background: #3b3b3b;
+  text-align: center;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.alert-box > p {
+  width: fit-content;
+}
+</style>
